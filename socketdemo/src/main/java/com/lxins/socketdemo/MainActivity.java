260f45ceb17c47b862cca6d36e985367613af12d
461146760@qq.com <461146760@qq.com>
2020-01-02 19:53:00 +0800

    add ...

--- socketdemo/src/main/java/com/lxins/socketdemo/MainActivity.java
+++ socketdemo/src/main/java/com/lxins/socketdemo/MainActivity.java
@@ -1,19 +1,41 @@
 package com.lxins.socketdemo;
 
+import android.icu.text.UnicodeSetSpanner;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.InputFilter;
+import android.text.Spanned;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private EditText editTextIP;
     private EditText editTextPort;
+    private EditText sendMsg;
+    private TextView receiveMsg;
 
     private String serverIP;
-    private String serverPort;
+    private int serverPort;
+    private SocketClient socketClient;
+    private boolean connection_state = false;
+    private Button connect_btn;
+    private Button send_btn;
+    private CheckBox hex_or_string;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +45,46 @@ public class MainActivity extends AppCompatActivity {
         initViews();
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.connect_btn:
+                if (connection_state) {
+                    Toast.makeText(MainActivity.this, "Server connected!", Toast.LENGTH_SHORT).show();
+                    socketClient.disconnect();
+                    connect_btn.setText(R.string.connect_btn);
+                    connection_state = false;
+                } else {
+
+                    socketClient.connect();
+                    Toast.makeText(MainActivity.this, "Server[" + serverIP + ":" + serverPort + "]", Toast.LENGTH_SHORT).show();
+                    connection_state = true;
+                    connect_btn.setText(R.string.disconnect_btn);
+                }
+                break;
+            case R.id.send_btn:
+                if (connection_state) {
+                    socketClient.send("OK!");
+                    Toast.makeText(MainActivity.this, "OK", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Do not connected!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.hex_or_string:
+                if (hex_or_string.isChecked()) {
+                    Toast.makeText(MainActivity.this, "Hex", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "String", Toast.LENGTH_SHORT).show();
+                }
+
+                sendMsg.setSelection(sendMsg.getText().length());
+                break;
+        }
+    }
+
     private void initViews() {
         editTextIP = findViewById(R.id.server_ip_etxt);
+        editTextIP.setSelection(editTextIP.getText().length());
         serverIP = editTextIP.getText().toString();
         editTextIP.addTextChangedListener(new TextWatcher() {
             @Override
@@ -44,7 +104,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         editTextPort = findViewById(R.id.server_port_etxt);
-        serverPort = editTextPort.getText().toString();
+        serverPort = Integer.parseInt(editTextPort.getText().toString());
         editTextPort.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -58,19 +118,55 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
-                serverPort = editTextPort.getText().toString();
+                serverPort = Integer.parseInt(editTextPort.getText().toString());
             }
         });
 
-        findViewById(R.id.connect_btn).setOnClickListener(new View.OnClickListener() {
+
+        connect_btn = findViewById(R.id.connect_btn);
+        if (connection_state) {
+            connect_btn.setText(R.string.disconnect_btn);
+        } else {
+            connect_btn.setText(R.string.connect_btn);
+        }
+
+        socketClient = new SocketClient(serverIP, serverPort, MainActivity.this);
+
+        connect_btn.setOnClickListener(this);
+
+        send_btn = findViewById(R.id.send_btn);
+        send_btn.setOnClickListener(this);
+
+        hex_or_string = findViewById(R.id.hex_or_string);
+        hex_or_string.setOnClickListener(this);
+
+        sendMsg = findViewById(R.id.send_msg);
+        sendMsg.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View v) {
-                switch (v.getId()) {
-                    case R.id.connect_btn:
-                        Toast.makeText(MainActivity.this, "Server[" + serverIP + ":" + serverPort + "]", Toast.LENGTH_SHORT).show();
-                        break;
-                }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
             }
         });
+
+        receiveMsg = findViewById(R.id.receive_msg);
+    }
+
+    public static String stringFilter(String str) throws PatternSyntaxException {
+        // 只允许字母、数字和汉字
+        //String regEx = "[^a-zA-Z0-9\u4E00-\u9FA5]";
+        String regEx = "[^a-fA-F0-9]";
+        Pattern p = Pattern.compile(regEx);
+        Matcher m = p.matcher(str);
+        return m.replaceAll("").trim();
     }
 }
---
+++ socketdemo/src/main/java/com/lxins/socketdemo/SocketClient.java
@@ -0,0 +1,99 @@
+package com.lxins.socketdemo;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * Created by pc on 2020/1/2.
+ */
+
+public class SocketClient {
+    private static final String TAG = "SocketClient";
+    private String host;
+    private int port;
+    private PrintWriter printWriter;
+    private BufferedReader bufferedReader;
+    private ExecutorService mExecutorService = null;
+    private String receiveMsg;
+    private AppCompatActivity uiAcitivity;
+
+    public SocketClient(String host, int port, AppCompatActivity uiAcitivty) {
+        this.host = host;
+        this.port = port;
+        this.uiAcitivity = uiAcitivty;
+        mExecutorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() * 4);
+    }
+
+    public void connect() {
+        mExecutorService.execute(new connectService());
+    }
+
+    public void send(String sendMsg) {
+        mExecutorService.execute(new sendService(sendMsg));
+    }
+
+    public void disconnect() {
+        mExecutorService.execute(new sendService("0"));
+    }
+
+    private class sendService implements Runnable {
+        private String msg;
+
+        sendService(String msg) {
+            this.msg = msg;
+        }
+
+        @Override
+        public void run() {
+            printWriter.println(this.msg);
+        }
+    }
+
+    private class connectService implements Runnable {
+        @Override
+        public void run() {
+            try {
+                Socket socket = new Socket(host, port);
+                socket.setSoTimeout(10000);
+                printWriter = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+                bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+                receiveMsg();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void receiveMsg() {
+        try {
+            while (true) {
+                if ((receiveMsg = bufferedReader.readLine()) != null) {
+                    Log.d(TAG, "receiveMsg: " + receiveMsg);
+                    uiAcitivity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            TextView textView = uiAcitivity.findViewById(R.id.receive_msg);
+                            textView.setText(receiveMsg);
+                        }
+                    });
+                }
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "receiveMsg: ");
+            e.printStackTrace();
+        }
+    }
+}
